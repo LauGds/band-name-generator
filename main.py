0dba3ddf2ce805097f0457f71bc99c93f6bd4791
LauGds <137927575+LauGds@users.noreply.github.com>
2023-07-04 11:51:33 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #1. Create a greeting for your program.
 print ("Welcome to your band name generator")
+
 #2. Ask the user for the city that they grew up in.
 city=input("What is the city that you grew up? ")
 print(city)
+
 #3. Ask the user for the name of a pet.
 pet=input("What is the name of your pet? ")
 print(pet)
+
 #4. Combine the name of their city and pet and show them their band name.
 print("The best name for your band is " + city + " " + pet)
